7c773d683c68cd26890dce2331a65aed6872fa5f
LindaTancini <lindatancini95@gmail.com>
2025-03-04 15:55:15 +0100

    feat: add elementButton and textcontent

--- main.js
+++ main.js
@@ -3,17 +3,24 @@ console.log("Ciao Linda");
 const elementLamp = document.getElementById("lamp");
 console.log(elementLamp);
 
+const elementButton = document.getElementById("btn");
+console.log(elementButton);
+
 // la lampadina è  spenta, creo una variabile inizialmente falsa
 let on = false;
 
-elementLamp.addEventListener("click", function () {
-  console.log("Ho cliccato sulla lampadina");
-
+// se metto elementLamp si accende la lampadina se clicco sopra l'img della lampadina
+// con elementButton la lampadina si accende se clicco sul bottone
+elementButton.addEventListener("click", function () {
+  //console.log("Ho cliccato sulla lampadina")
+  console.log("Ho cliccato sul bottone");
   // cliccando, la lampadina si accende
   if (on) {
     elementLamp.src = "img/white_lamp.png";
+    elementButton.textContent = "Accendi";
   } else {
     elementLamp.src = "img/yellow_lamp.png";
+    elementButton.textContent = "Spegni";
   }
   // con la negazione, alterno lo stato di accensione della lampadina
   on = !on;
