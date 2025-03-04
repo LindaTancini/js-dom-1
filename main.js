264ecfad6ef0d350b50fe5bff531051ca9f86da4
LindaTancini <lindatancini95@gmail.com>
2025-03-04 14:17:06 +0100

    feat: add click on elementLamp

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ console.log(elementLamp);
 // la lampadina è  spenta, creo una variabile inizialmente falsa
 
 let on = false;
+
+elementLamp.addEventListener("click", function () {
+  console.log("clicco su lampadina");
+});
