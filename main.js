5a326794e6a9d985f0714b7a76c13fb2e67dcbed
LindaTancini <lindatancini95@gmail.com>
2025-03-04 14:11:48 +0100

    feat: correct "lampadina" with "lamp" and creat boolean variable and elementLamp

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("Ciao Linda");
+
+const elementLamp = document.getElementById("lamp");
+console.log(elementLamp);
+
+// la lampadina è  spenta, creo una variabile inizialmente falsa
+
+let on = false;
