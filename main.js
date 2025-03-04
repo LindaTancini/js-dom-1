79a5dfcb59db7a4850f78d8922c1407284bd8b34
LindaTancini <lindatancini95@gmail.com>
2025-03-04 14:41:54 +0100

    feat: added (if, else) to turn on the light bulb

--- main.js
+++ main.js
@@ -4,9 +4,17 @@ const elementLamp = document.getElementById("lamp");
 console.log(elementLamp);
 
 // la lampadina è  spenta, creo una variabile inizialmente falsa
-
 let on = false;
 
 elementLamp.addEventListener("click", function () {
-  console.log("clicco su lampadina");
+  console.log("Ho cliccato sulla lampadina");
+
+  // cliccando, la lampadina si accende
+  if (on) {
+    elementLamp.src = "img/white_lamp.png";
+  } else {
+    elementLamp.src = "img/yellow_lamp.png";
+  }
+  // con la negazione, alterno lo stato di accensione della lampadina
+  on = !on;
 });
